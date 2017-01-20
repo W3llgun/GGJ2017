6acd06485dba8458cf5d62a680bc2641e8fe5feb
William <william.pat@free.fr>
2017-01-20 18:57:32 +0100

    Add basic IA

---
+++ Assets/Scripts/IA/IA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class IA : MonoBehaviour {
+    protected NavMeshAgent agent;
+    
+
+	// Use this for initialization
+	void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
