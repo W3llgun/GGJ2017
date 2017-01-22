70360749fbbc0c8663f8dd1f4b3c0ba658fc20a9
ETU\e.buglione <e.buglione@rubika-edu.com>
2017-01-22 13:31:57 +0100

    Update graphique

---
+++ Assets/Scripts/IA/Death.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public List<Transform> rigidbodyList;
+    public ParticleSystem ps1;
+    public ParticleSystem ps2;
+
+	public void LaunchDeath ()
+    {
+        StartCoroutine(startFall());	
+	}
+
+    IEnumerator startFall()
+    {
+        yield return new WaitForSeconds(0.9f);
+        foreach (Transform itemToDrop in rigidbodyList)
+        {
+            itemToDrop.parent = transform.root;
+            Rigidbody rb = itemToDrop.gameObject.AddComponent<Rigidbody>();
+        }
+
+        yield return new WaitForSeconds(1.35f);
+        this.gameObject.AddComponent<Rigidbody>();
+    }
+}
